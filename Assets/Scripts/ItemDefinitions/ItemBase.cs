05e1908d342830381be13c1de5fe726ddd4246b3
Simon Catanese <simoncatanese.pro@gmail.com>
2024-11-22 10:46:10 +0100

    items classes wip

---
+++ Assets/Scripts/ItemDefinitions/ItemBase.cs
@@ -0,0 +1,24 @@
+using UnityEngine.Events;
+
+public abstract class ItemBase
+{
+    public static bool IsUnlocked { get; private set; } = false;
+
+
+    
+    public static void Unlock()
+    {
+        if (IsUnlocked) return;
+
+        IsUnlocked = true;
+        OnItemUnlocked.Invoke();
+    }
+
+#region EVENTS
+
+    public static UnityEvent OnItemUnlocked { get; set; } = new();
+    public static UnityEvent OnItemUsed { get; set; } = new();
+
+#endregion
+
+}
