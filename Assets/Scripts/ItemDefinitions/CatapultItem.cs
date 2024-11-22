1526f7ab1636ffb379e228219f91405e04c93262
AdrienLln <alesluin@gaming.tech>
2024-11-22 21:33:27 +0100

    ADD/player moveanim

--- Assets/Scripts/ItemDefinitions/CatapultItem.cs
+++ Assets/Scripts/ItemDefinitions/CatapultItem.cs
@@ -2,6 +2,7 @@ public class CatapultItem : ItemBase
 {
     public override string GetDescription()
     {
-        return "Pas de description";
+        return "Une catapulte bricolée où la gomme sert de base et la règle comme levier." +
+            " Conçue pour propulser le petit soldat lui-même, elle lui permet d’atteindre des endroits inaccessibles.";
     }
 }
