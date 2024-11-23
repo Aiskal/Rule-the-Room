2d714238b9fbc584bbee9da6bd08abfb84a5b1b7
aiskal <icingdawn@gmail.com>
2024-11-23 09:55:46 +0100

    START/Description

--- Assets/Scripts/Inventory/Inventory Slots/CatapultSlot.cs
+++ Assets/Scripts/Inventory/Inventory Slots/CatapultSlot.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CatapultSlot : BaseInventorySlot
@@ -15,5 +16,23 @@ public class CatapultSlot : BaseInventorySlot
         if (CatapultItem.IsUnlocked) return;
 
         base.ItemClicked();
+    }
+
+    public override void UpdateDescription(GameObject description)
+    {
+        if (description == null)
+        {
+            Debug.LogError("Description GameObject est null !");
+            return;
+        }
+
+        var textTMP = description.transform.Find("Text")?.GetComponent<TextMeshProUGUI>();
+        if (textTMP == null)
+        {
+            Debug.LogError("TextMeshProUGUI introuvable dans l'objet description.");
+            return;
+        }
+
+        textTMP.text = CatapultItem.GetDescription();
     }
 }
